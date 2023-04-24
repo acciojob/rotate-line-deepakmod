0e7757345ee3d53688b9d43beee49166b5868640
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-24 11:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+    var style = document.createElement("style");
+    style.textContent = "#line{animation: rotate 4s;"+
+        "animation-fill-mode: forwards;"+
+        "animation-timing-function:linear;}"+
+        "@keyframes rotate{"+
+            "from{transform: rotate(0deg);}"+
+            "to{transform: rotate(500deg);}"+
+        "}";
+    document.body.appendChild(style);
